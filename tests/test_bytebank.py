26d23066e563ed415e47407ac0cafe2a3623acf6
Bruno <brox.fs@gmail.com>
2023-02-23 20:32:29 -0300

    Testing Exception errors and decorating tests with marks.

--- tests/test_bytebank.py
+++ tests/test_bytebank.py
@@ -1,7 +1,10 @@
 from codigo.bytebank import Funcionario
+import pytest
+from pytest import mark
 
 
 class TestClass:
+    @mark.calcular_bonus
     def test_quando_idade_recebe_13_03_2000_deve_retornar_23(self):
         entrada = '13/03/2000'  # given
         esperado = 23
@@ -11,6 +14,7 @@ class TestClass:
 
         assert resultado == esperado  # then
 
+    @mark.calcular_bonus
     def test_quando_sobrenome_recebe_Bruno_Guerreiro_de_Queiroz_deve_retornar_Guerreiro_de_Queiroz(self):
         entrada = ' Bruno Guerreiro de Queiroz  '
         esperado = 'Guerreiro de Queiroz'
@@ -30,3 +34,20 @@ class TestClass:
 
         assert resultado == esperado
 
+    def test_quando_calcular_bonus_recebe_1000_deve_retornar_100(self):
+        entrada = 1000
+        esperado = 100
+
+        ana = Funcionario('Ana', '11/10/2000', entrada)
+        resultado = ana.calcular_bonus()
+
+        assert resultado == esperado
+
+    def test_quando_calcular_bonus_recebe_10000000_deve_retornar_ExceptionError(self):
+        with pytest.raises(Exception):
+            entrada = 10000000
+
+            ana_rica = Funcionario('Ana', '11/10/2000', entrada)
+            resultado = ana_rica.calcular_bonus()
+
+            assert resultado
