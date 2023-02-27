c3fd83f58c4e772cb389bdfcd3697b00e95c78f8
Bruno <brox.fs@gmail.com>
2023-02-26 22:14:48 -0300

    Cleaned file.

--- codigo/main.py
+++
@@ -1,8 +0,0 @@
-from codigo.bytebank import Funcionario
-
-
-def test_idade():
-    funcionario_test = Funcionario('Teste', '13/03/2000', 1111)
-    print(f'Teste = {funcionario_test.idade()}')
-
-test_idade()
